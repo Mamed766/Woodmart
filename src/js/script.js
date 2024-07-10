1a840922feb6a41d08486a1779f3b3480192ea61
Mamed766 <mehemmeddemirli57@gmail.com>
2024-07-10 18:00:02 +0400

    featured: created structure of products

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,26 @@ var swiper = new Swiper(".mySwiper", {
     clickable: false,
   },
 });
+
+var newSwiper = new Swiper(".productSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
